1ed68ae0ecc9cec8b49a84fe4d70b6a8970b47db
mjpitkin45 <mjpitkin45@gmail.com>
2022-10-06 20:15:47 +0100

    added footer and featured album components

---
+++ mern-skeleton/client/src/components/FeaturedAlbums/FeaturedAlbums.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturedAlbums() {
+  return (
+    <div><p>FeaturedAlbums</p></div>
+  )
+}
+
+export default FeaturedAlbums
--- mern-skeleton/client/src/pages/HomePage/index.js
+++ mern-skeleton/client/src/pages/HomePage/index.js
@@ -1,8 +1,9 @@
+import FeaturedAlbums from "../../components/FeaturedAlbums/FeaturedAlbums";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 export default function HomePage() {
   return <div>
     <SearchBar />
-    <p>Featured Albums</p>
+    <FeaturedAlbums />
   </div>;
 }
