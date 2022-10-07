49e9dc42fd8e182d158c887fa2ba2d318b6fdd85
Ryan Cooper <ryangcooper96@gmail.com>
2022-10-07 12:12:39 +0100

    Created BreadCrumb component directory and added relevant files - imported into Marketplace page. Also made many css tweaks across Marketplace page and it's child components

---
+++ mern-skeleton/client/src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./BreadCrumbs.css"
+
+function BreadCrumbs() {
+  return (
+    <div className='BreadCrumbs'>BreadCrumbs &gt; Crumb &gt; Crumb</div>
+  )
+}
+
+export default BreadCrumbs
--- mern-skeleton/client/src/components/ResultCard/ResultCard.jsx
+++ mern-skeleton/client/src/components/ResultCard/ResultCard.jsx
@@ -7,11 +7,11 @@ function ResultCard() {
         <div className='ResultCard-favourite'><span className="material-symbols-outlined">favorite</span></div>
         <div className='ResultCard-content'>
             <div className='ResultCard-image'>
-                <img src="" alt="blah"/>
+                <img src="https://www.nme.com/wp-content/uploads/2016/10/DefinitelyMaybeOasis2PR200712_0.jpg" alt="blah"/>
             </div>
             <div className='ResultCard-text'>
                 <h2>Oasis</h2>
-                <h3>Wonderwall</h3>
+                <h3>Definitely Maybe</h3>
             </div>
         </div>
     </div>
