1f2b1005685b7f9880b56efb4e5c2276e3c6a2cd
Ryan Cooper <ryangcooper96@gmail.com>
2022-10-07 16:19:57 +0100

    Initial attempt at breadcrumbs using react-router-dom's useLocation function

--- mern-skeleton/client/src/components/BreadCrumbs/BreadCrumbs.jsx
+++ mern-skeleton/client/src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import "./BreadCrumbs.css"
 
 function BreadCrumbs() {
+
+  const location = useLocation();
+  const crumbs = (location.pathname.split('/').filter((crumb) => (crumb !== '')).map((crumb) => (crumb[0].toUpperCase()+crumb.slice(1))))
+  console.log(crumbs)
+  
+
   return (
-    <div className='BreadCrumbs'>BreadCrumbs &gt; Crumb &gt; Crumb</div>
+    <div className='BreadCrumbs'>{crumbs.map((crumb) => (<><span> &gt; </span><span>{crumb}</span></>))}</div>
   )
 }
 
