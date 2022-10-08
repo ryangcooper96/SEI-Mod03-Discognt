7889d1f674a2a3303df7ed8fea82a528a77fdc7e
mjpitkin45 <mjpitkin45@gmail.com>
2022-10-08 17:18:36 +0100

    fixed css error: properties being applied to elements other than billing page ones

--- mern-skeleton/client/src/pages/Dashboard/Billing/Billing.jsx
+++ mern-skeleton/client/src/pages/Dashboard/Billing/Billing.jsx
@@ -22,7 +22,7 @@ function Billing() {
     }
   return (
       <div>   
-        <form>
+        <form className='billingForm'>
               <select onChange={handleChange} value={state.paymentMethod} name='paymentMethod'>
                 <option value="">--Payment Method--</option>
                 <option value="visa">Visa</option>
@@ -32,14 +32,14 @@ function Billing() {
 {/* Okay this one here needs to be a dropdown to pick from a set of options, fix that */}
 {state.paymentMethod !== 'paypal' ? 
                   (<div className='creditcard-wrapper'><input type='text' placeholder='Name on card' value={state.nameOnCard} onChange={handleChange} name='nameOnCard'></input>
-                      <input type='text' placeholder='card number' value={state.cardNumber} onChange={handleChange} name='cardNumber'></input>
-                      <input type='text' placeholder='security code' value={state.securityCode} onChange={handleChange} name='securityCode'></input>
-                      <input type='text' placeholder='billing address line 1' value={state.address1} onChange={handleChange} name='address1'></input>
-                      <input type='text' placeholder='billing address line 2' value={state.address2} onChange={handleChange} name='address2'></input>
-                      <input type='text' placeholder='billing address line 3' value={state.address3} onChange={handleChange} name='address3'></input>
-                      <input type='text' placeholder='billing address zip/postal code' value={state.postcode} onChange={handleChange} name='postcode'></input>
+                      <input className='billingElement' type='text' placeholder='card number' value={state.cardNumber} onChange={handleChange} name='cardNumber'></input>
+                      <input type='text' className='billingElement'  placeholder='security code' value={state.securityCode} onChange={handleChange} name='securityCode'></input>
+                      <input type='text' className='billingElement'  placeholder='billing address line 1' value={state.address1} onChange={handleChange} name='address1'></input>
+                      <input type='text' className='billingElement' placeholder='billing address line 2' value={state.address2} onChange={handleChange} name='address2'></input>
+                      <input type='text' className='billingElement' placeholder='billing address line 3' value={state.address3} onChange={handleChange} name='address3'></input>
+                      <input type='text' className='billingElement' placeholder='billing address zip/postal code' value={state.postcode} onChange={handleChange} name='postcode'></input>
               
-            <button>submit</button></div>) :
+                      <button className='billingElement'>submit</button></div>) :
             (<button>submit</button>)
             }
         </form>   
