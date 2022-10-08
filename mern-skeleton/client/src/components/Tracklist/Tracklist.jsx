e847aa1afe7c7d9de50b8a209c19fca741c6dcd9
Ryan Cooper <ryangcooper96@gmail.com>
2022-10-08 11:28:12 +0100

    Created basic skeleton for ProductPage and created ImageViewer & Tracklist component directories with relevant files. ImageViewer static component ready.

---
+++ mern-skeleton/client/src/components/Tracklist/Tracklist.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Tracklist.css"
+
+function Tracklist() {
+  return (
+    <div>Tracklist</div>
+  )
+}
+
+export default Tracklist
--- mern-skeleton/client/src/pages/ProductPage/ProductPage.jsx
+++ mern-skeleton/client/src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs'
+import ImageViewer from '../../components/ImageViewer/ImageViewer'
+import Tracklist from '../../components/Tracklist/Tracklist'
 import "./ProductPage.css"
 
 function ProductPage() {
   return (
-    <div>ProductPage</div>
+    <div className='ProductPage'>
+        <BreadCrumbs />
+        <div className='ProductPage-productContainer'>
+            <div className='ProductPage-productInfo'>
+                <h1>Product Title</h1>
+                <h2>Artist</h2>
+                <h3>Release Date: 1996</h3>
+                <h3>Record Label</h3>
+                <p>Find exactly what you're looking for in our marketplace... </p>
+            </div>    
+            <ImageViewer />
+            <Tracklist />
+            <div className='Listings'></div>
+        </div>
+    </div>
   )
 }
 
