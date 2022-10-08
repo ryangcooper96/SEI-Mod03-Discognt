433477a640b7cad4bbd14f40d1ec4dfbab0c4ffa
Ryan Cooper <ryangcooper96@gmail.com>
2022-10-08 16:15:07 +0100

    Created Listings & ListingCard component directory with relevant files, built and imported into ProductPage

--- mern-skeleton/client/src/components/Tracklist/Tracklist.jsx
+++ mern-skeleton/client/src/components/Tracklist/Tracklist.jsx
@@ -3,7 +3,68 @@ import "./Tracklist.css"
 
 function Tracklist() {
   return (
-    <div>Tracklist</div>
+    <div className='Tracklist'>
+        <h2>Tracklist</h2>
+    <table className='Tracklist-table'>
+        <thead>
+            <th>#</th>
+            <th>Song</th>
+            <th className='duration'>Duration</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>Song</td>
+                <td className='duration'>0:00</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
   )
 }
 
--- mern-skeleton/client/src/pages/ProductPage/ProductPage.jsx
+++ mern-skeleton/client/src/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs'
 import ImageViewer from '../../components/ImageViewer/ImageViewer'
 import Tracklist from '../../components/Tracklist/Tracklist'
+import Listings from "../../components/Listings/Listings"
 import "./ProductPage.css"
 
 function ProductPage() {
@@ -10,15 +11,16 @@ function ProductPage() {
         <BreadCrumbs />
         <div className='ProductPage-productContainer'>
             <div className='ProductPage-productInfo'>
+            <div className='ProductPage-favourite'><span className="material-symbols-outlined">favorite</span></div>
                 <h1>Product Title</h1>
                 <h2>Artist</h2>
                 <h3>Release Date: 1996</h3>
                 <h3>Record Label</h3>
-                <p>Find exactly what you're looking for in our marketplace... </p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum architecto obcaecati perspiciatis maxime velit omnis! Neque, accusantium porro, voluptatem accusamus dolorum doloremque repellat iure laborum nisi exercitationem quo fugiat adipisci.</p>
+            <Tracklist />
             </div>    
             <ImageViewer />
-            <Tracklist />
-            <div className='Listings'></div>
+            <Listings />
         </div>
     </div>
   )
