57f908fcca789b8d2a633d2031ac6d0971c2c465
mjpitkin45 <mjpitkin45@gmail.com>
2022-10-13 20:18:30 +0100

    began adding stripe integration for payments, also listing and wishlist models

---
+++ mern-skeleton/server/routes/api/stripe.js
@@ -0,0 +1,3 @@
+import { Express } from "express";
+import Stripe from "stripe";
+const stripe = new Stripe('sk_test_51LsWKuK3m4JInnwokp2NxjstpHVwkXiaVLHkVfkhaKERM0LXZDN6rjg172wdFmkGGVMmQBv15teAgATMIix7C5bx00Ll22wHev')
