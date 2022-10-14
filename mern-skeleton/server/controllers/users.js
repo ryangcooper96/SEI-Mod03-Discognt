139396ba05f89ad4538110e573e811fce38be0c1
zsuzsanna mozsary <mozsaryzsuzsanna@gmail.com>
2022-10-14 14:09:51 +0100

    password bugfix

--- mern-skeleton/server/controllers/users.js
+++ mern-skeleton/server/controllers/users.js
@@ -21,6 +21,7 @@ export async function signup(req, res) {
 
 export async function login(req, res) {
   try {
+    // console.log("this is the req:", req);
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(401).json({ err: "bad credentials" });
     user.comparePassword(req.body.password, (err, isMatch) => {
