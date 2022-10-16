96d9e2e71ebd3c7b7f9231f5b89d46fdcfa0098f
zsuzsanna mozsary <mozsaryzsuzsanna@gmail.com>
2022-10-16 21:03:08 +0100

    bugfix

--- mern-skeleton/client/src/pages/Dashboard/Listings/Listing/Listing.jsx
+++ mern-skeleton/client/src/pages/Dashboard/Listings/Listing/Listing.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import listingService from '../../../../utils/listingService'
+import React from "react";
+import listingService from "../../../../utils/listing";
 
 function Listing(props) {
-    const handleDelete = async (e) => {
-        try {
-            await listingService.deleteListing(e);
-        } catch (error) {
-            console.log(error)
-        }
+  const handleDelete = async (e) => {
+    try {
+      await listingService.deleteListing(e);
+    } catch (error) {
+      console.log(error);
     }
-    const handleUpdate = async (e) => {
-        try {
-            await listingService.updateListing(e);
-        } catch (error) {
-            console.log(error)
-        }
+  };
+  const handleUpdate = async (e) => {
+    try {
+      await listingService.updateListing(e);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    
-    return (
-        <div><h4>Title: {props.title}</h4><p>Condition: {props.condition}, price: £{`${props.price}`}, description: {props.description}</p><button onClick={handleUpdate(props.listingId) }>Update</button><button onClick={handleDelete(props.listingId)}>Delete</button></div>
-    )
+  return (
+    <div>
+      <h4>Title: {props.title}</h4>
+      <p>
+        Condition: {props.condition}, price: £{`${props.price}`}, description:{" "}
+        {props.description}
+      </p>
+      <button onClick={handleUpdate(props.listingId)}>Update</button>
+      <button onClick={handleDelete(props.listingId)}>Delete</button>
+    </div>
+  );
 }
 
-export default Listing
+export default Listing;
