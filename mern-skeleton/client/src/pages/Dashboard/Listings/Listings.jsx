5e034c8c1d3bc4bc6cddca19d47b5eed57a7f11d
mjpitkin45 <mjpitkin45@gmail.com>
2022-10-17 14:18:29 +0100

    began integrating backend for listing dashboard

--- mern-skeleton/client/src/pages/Dashboard/Listings/Listings.jsx
+++ mern-skeleton/client/src/pages/Dashboard/Listings/Listings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Listing from './Listing/Listing'
-import { useState } from 'react'
+import listing from '../../../utils/listing.js'
+import { useState, useEffect } from 'react'
 
 function ListingsPage() {
   //fetch listings data
@@ -31,6 +32,11 @@ function ListingsPage() {
       price: 0,
       description: 'test'
     }])
+
+useEffect(() => {
+setListings(listing.getListingsbyLoggedInUser());
+console.log(listings);
+}, [])
   return (
     <div>{listings.map((item) => {
       return (<Listing listingId={item.listingId} title={item.title} format={item.format} condition={item.condition} price={item.price} description={item.description} />)
